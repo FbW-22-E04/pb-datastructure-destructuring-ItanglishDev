001b8ff8b21345b53b2a6c6fe6c2218f78a40786
chris <chrishethertonweb@gmail.com>
2022-08-11 14:27:37 +0200

    ex 2 done

---
+++ index.js
@@ -0,0 +1,19 @@
+// Ex 1
+
+const fruit = "banana"
+const vegetable = "cucumber"
+const food = ["bread", "cakes", "pizza"]
+
+// Ex 2
+
+
+const costumes = {
+  fran: "witch",
+  mazen: "clown",
+  antonio: "queen"
+}
+
+const fran = costumes.fran
+const mazen = costumes.mazen
+const antonio = costumes.antonio
+console.log(fran, mazen, antonio);
