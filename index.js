7288f7879336d6903d07e13097a164d1c8edda9a
chris <chrishethertonweb@gmail.com>
2022-08-11 14:36:11 +0200

    ex 3 done

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ const fran = costumes.fran
 const mazen = costumes.mazen
 const antonio = costumes.antonio
 console.log(fran, mazen, antonio);
+
+// Ex 3
+
+const bandMusician = {
+  nameBand: "Red Hot Vhilli",
+  hit: "sweet child of mine",
+  nationality: "god knows",
+  genre: "who cares"
+}
+
+function displayAll() {
+  console.log(`${bandMusician.nameBand} is a ${bandMusician.nationality} singer/songwriter.The musician sings ${bandMusician.genre} and their greatest hits is ${bandMusician.hit}`);
+}
+
+displayAll()
